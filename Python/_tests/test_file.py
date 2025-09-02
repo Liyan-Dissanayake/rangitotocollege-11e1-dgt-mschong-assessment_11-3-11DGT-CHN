9fcedcfcdccaa9e12d29f5339c279a8936dc074d
Liyan Dissanayake <198438@cloud.rangitoto.school.nz>
2025-09-03 11:00:34 +1200

    fxes

--- Python/_tests/test_file.py
+++ Python/_tests/test_file.py
@@ -10,6 +10,6 @@ player_score = 0
 computer_score = 0
 
 title_label = tk.Label(root, text="Rock-Paper-Scissors", font=("Arial", 16))
-title_label.pack(pady=200, padx=1)
+title_label.pack(pady=10, padx=(1,0))
 
 root.mainloop()
